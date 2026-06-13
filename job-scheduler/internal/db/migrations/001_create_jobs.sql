3677fb4a1094fe8ed239ea423cdda0979b5d0cc0
Vivek chaurasia <vivekkumarr221106@gmail.com>
2026-06-14 00:53:01 +0530

    trigger function we dont havr to give timestamp intentionally it will handle it

--- job-scheduler/internal/db/migrations/001_create_jobs.sql
+++ job-scheduler/internal/db/migrations/001_create_jobs.sql
@@ -4,7 +4,7 @@ CREATE TYPE job_status AS ENUM {
     'completed',
     'failed',
     'dead'
-}
+};
 
 CREATE TABLE jobs {
     id              BIGSERIAL           PRIMARY KEY,
@@ -18,5 +18,27 @@ CREATE TABLE jobs {
     run_at          TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
     created_at      TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
     updated_at      TIMESTAMPTZ  NOT NULL DEFAULT NOW()
-}
+};
 
+
+CREATE INDEX idx_jobs_scheduler
+    ON jobs (priority DESC, run_at ASC, created_at ASC)
+    WHERE status IN ('pending', 'failed');
+
+CREATE INDEX idx_jobs_watchdog
+    ON jobs (locked_until ASC)
+    WHERE status = 'running';
+
+
+CREATE OR REPLACE FUNCTION trigger_set_updated_at()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER set_updated_at
+    BEFORE UPDATE ON jobs
+    FOR EACH ROW
+    EXECUTE FUNCTION trigger_set_updated_at();
